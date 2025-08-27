d5d6b0c3ccb0e07880b0609697193e8dbc8b0a31
asepjamaludinn <asepjamaludinn24@gmail.com>
2025-08-27 11:09:45 +0700

    resolve conflict

--- src/routes/notificationReadRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { markAsRead } from "../controllers/notificationReadController.js";
-const router = express.Router();
-
-router.post("/", markAsRead);
-
-export default router;
